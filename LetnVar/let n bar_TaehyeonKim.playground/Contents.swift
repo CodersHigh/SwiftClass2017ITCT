0edbbecbd50224d8306a261ff93b23a4e2bfe561
June <31872218+wimara0228@users.noreply.github.com>
2017-09-19 10:25:36 +0900

    merge
    pull

---
+++ LetnVar/let n bar_TaehyeonKim.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+let name = "TaehyeonKim"
+var weigh = 100.0
+weigh = 50
+/*let은 기본적으로 변경이 불가능한 상수를 만들고, var는 수정이 가능한 변수를 만든다.*/
+
+let weightLog = [100.0, 95.0, 93.0]
+/* weightLog.append(30.0) 에러가 난다.
+배열도 마찬가지로 변경을 할 수 없다. 상수이기 때문이다.*/
+
